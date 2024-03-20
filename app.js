6ec637e1009327fe0f84c4c6296dcb325c41f84f
40022808 <70860816+40022808@users.noreply.github.com>
2024-03-20 10:28:32 +0100

    Add files via upload

--- app.js
+++ app.js
@@ -4,10 +4,20 @@ const negy = document.getElementById("negy");
 const button = document.getElementById("button");
 const ot = document.getElementById("ot");
 
+// 将 abc 声明为全局变量
+var abc = 1;
+var nev = "";
 
-button.addEventListener("click",() => {
-    let num = negy.value;
-    ot.textContent = (num);
-    negy.value= "";
-})
 
+button.addEventListener("click", () => {
+    if (abc === 1) {
+        nev = negy.value;
+        ot.textContent = "你好" + nev + "!";
+        negy.value = "";
+        abc = 2; // 更新 abc 的值
+        keto.textContent += "你好" + nev + "!";
+    } 
+    else if (abc === 2) {
+        
+    }
+});
